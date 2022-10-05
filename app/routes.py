d870d76252fecc63896922684ea9c64ada791a36
Dat Le <tiendat.le129@gmail.com>
2022-10-05 13:46:33 +0700

    update routes

--- app/routes.py
+++ app/routes.py
@@ -1,63 +1,90 @@
 from flask import render_template
 from app import app
-from flask import json, render_template, request, redirect
+from flask import json, render_template, jsonify, request
 from flask.json import jsonify
 import numpy as np
 import cv2
 from app.ocr.detection import Detector
 from app.ocr import extract_infos
-from flask.helpers import url_for
+from app.ocr.functions import load_labels_map, stringToRGB
 import os
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/get_extract", methods=["POST"])
-def get_extract():
-    pass
+import tensorflow as tf
 
 THRESHOLD = 0.3
 imageSize = 512
 seq2seq_url = 'app/ocr/vietocr/seq2seqocr.pth'
 targetSize = { 'w': imageSize, 'h': imageSize }
+label_map_corner = load_labels_map('app/ocr/label_map/corner.pbtxt')
+label_map_text = load_labels_map('app/ocr/label_map/text.pbtxt')
+label_map_text_chip = load_labels_map('app/ocr/label_map/text_chip.pbtxt')
+#load keras model
+classifi = tf.keras.models.load_model('app/models/classification/classification.h5')
 
-def url_to_image(url):
-    """
-    Read image from url
-    """
-    # resp = urllib.request.urlopen(url)
-    # image = np.asarray(bytearray(resp.read()), dtype="uint8")
-    # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    image = cv2.imread(url)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    return image
+corner_chip = Detector('app/models/ctc_chip/corner/saved_model', THRESHOLD, targetSize, label_map_corner)
+text_chip = Detector('app/models/ctc_chip/text/saved_model', THRESHOLD, targetSize, label_map_text_chip)
+
+corner = Detector('app/models/id_card/corner/saved_model', THRESHOLD, targetSize, label_map_corner)
+text = Detector('app/models/id_card/text/saved_model', THRESHOLD, targetSize, label_map_text)
+
+ocr_model = extract_infos.ocr(seq2seq_url)
+
+detect_corner = None
+detect_text = None
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route('/api/v1/ocr', methods=['GET', 'POST'])
 def api_ocr():
-    # data = json.loads(request.data)
-    # img_url = data['img_url']
-    # corner_url = data['corner_url']
-    # text_url = data['text_url']
-    image = url_to_image('C:/Users/tiend/Downloads/facenet_embedding/datle1.JPG')
-    image = np.array(image)
+    # image = url_to_image('C:/Users/tiend/Downloads/image_f83d36fc-f415-41e1-999d-814d09528e9e.jpg')
+    
+    # image = np.array(image)
+    image = None
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        image = stringToRGB(data['img'])
+    
+    #classifi
+    img = cv2.resize(image, (150, 150))
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0
+    pred = classifi.predict(img)
+
     targetSize['h'] = image.shape[0]
     targetSize['w'] = image.shape[1]
 
-    detect_corner = Detector('C:/Users/tiend/Documents/ocr_id_card/app/models/ctc_chip/corner/saved_model', threshold=THRESHOLD, targetSize=targetSize)
-    detect_text = Detector('C:/Users/tiend/Documents/ocr_id_card/app/models/ctc_chip/text/saved_model', threshold=THRESHOLD, targetSize=targetSize)
-    # try:  
-    corner_img = detect_corner.detect_corner(image, THRESHOLD, targetSize)
-    # except:
-    #     return jsonify({'errorCode': 1, 'errorMessage': 'can not detect id card. Id card need contain 4 corners'})
+    
+    if pred[0][0] < 0.5:
+        detect_corner = corner_chip
+        detect_text = text_chip
+    else:
+        detect_corner = corner
+        detect_text = text
+
+    try:  
+        corner_img = detect_corner.detect_corner(image, THRESHOLD, targetSize)
+        cv2.imwrite('app/ocr/corner.jpg', corner_img)
+    except:
+        return jsonify({'errorCode': 1, 'errorMessage': 'can not detect id card. Id card need contain 4 corners'})
 
     targetSize['h'] = corner_img.shape[0]
     targetSize['w'] = corner_img.shape[1]
 
     textt = detect_text.detect_text(corner_img, THRESHOLD, targetSize)
 
-    test = extract_infos.ocr(seq2seq_url)
-
-    data = test.OCR(corner_img, textt)
-    
+    data = ocr_model.OCR(corner_img, textt)
+    print(data)
     return jsonify(data)
+
+
+# def url_to_image(url):
+#     """
+#     Read image from url
+#     """
+#     # resp = urllib.request.urlopen(url)
+#     # image = np.asarray(bytearray(resp.read()), dtype="uint8")
+#     # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+#     image = cv2.imread(url)
+#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     return image
