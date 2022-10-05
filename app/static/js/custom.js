17ecca1933bfea1dc9c1cfddc46b9b5d007cabc4
Dat Le <tiendat.le129@gmail.com>
2022-10-05 13:45:58 +0700

    update static, template

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -15,19 +15,46 @@ function readURL(input) {
 
 $(function () {
     $('#upload').on('change', function () {
-        readURL(input);
+        readURL($('#upload'));
     });
 });
 
 /*  ==========================================
     SHOW UPLOADED IMAGE NAME
 * ========================================== */
-var input = document.getElementById( 'upload' );
-var infoArea = document.getElementById( 'upload-label' );
+// var input = ;
+var infoArea = $('#upload-label');
 
-input.addEventListener( 'change', showFileName );
-function showFileName( event ) {
+
+function showFileName(event) {
   var input = event.srcElement;
   var fileName = input.files[0].name;
   infoArea.textContent = 'File name: ' + fileName;
 }
+
+$('#upload').bind('change',showFileName);
+$(function() {
+    $('#submit-button').click(function() {
+        // var form_data = new FormData($('#upload-img'));
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/ocr',
+            data:  JSON.stringify({img: $("#imageResult")[0].src.split(',')[1]}),
+            contentType: 'app/json',
+            cache: false,
+            processData: false,
+            success: function(data) {
+                console.log('Success!');
+                console.log(data);
+                $('#id').val(data['data']['data']['id']);
+                $('#name').val(data['data']['data']['name']);
+                $('#birthday').val(data['data']['data']['birthday']);
+                $('#nation').val(data['data']['data']['nation']);
+                $('#sex').val(data['data']['data']['sex']);
+                $('#exp').val(data['data']['data']['exp']);
+                $('#hometown').val(data['data']['data']['hometown']);
+                $('#addr').val(data['data']['data']['addr']);
+            },
+        });
+    });
+});
